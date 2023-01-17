e99425a90f3fba5493270dba45e65116810220db
Codeyg12 <codey.gallup@gmail.com>
2023-01-17 12:41:56 -0700

    UPDATE: adding titles and links

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,104 +8,170 @@ import {
   Hangman,
   Book,
 } from "./assets";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 export default function Project() {
   return (
     <div className="project-set">
       <div className="image-container">
-        <img src={ORM} />
+        <img src={ORM} alt="ORM" />
         <div className="overlay">
-          <div className="text">MySQL, Express, Insomnia</div>
+          <div className="text">
+            ORM E-Commerce
+            <br />
+            MySQL, Express, Insomnia
+          </div>
           <a
             href="https://github.com/Codeyg12/ORM-E-commerce"
             rel="noreferrer"
             target="_blank"
-            className="orm-proj"
-          ></a>
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faLink} />
+          </a>
         </div>
       </div>
       <div className="image-container">
-        <img src={Jate} alt="" />
+        <img src={Jate} alt="Jate" />
         <div className="overlay">
-          <div className="text"></div>
+          <div className="text">
+            Jate <br />
+            JavaScript, Express
+          </div>
           <a
             href="https://github.com/Codeyg12/expressNotes"
             rel="noreferrer"
             target="_blank"
-            className="note-proj"
-          ></a>
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faLink} />
+          </a>
         </div>
       </div>
       <div className="image-container">
-        <img src={TechTalk} alt="" />
+        <img src={TechTalk} alt="TechTalk" />
         <div className="overlay">
-          <div className="text"></div>
+          <div className="text">
+            TechTalk
+            <br />
+            Sequelize, MySQL, Express
+          </div>
           <a
             href="https://github.com/Codeyg12/MVC-tech-blog"
             rel="noreferrer"
             target="_blank"
-            className="tech-proj"
-          ></a>
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faLink} />
+          </a>
         </div>
       </div>
       <div className="image-container">
-        <img src={Scribe} alt="" />
+        <img src={Scribe} alt="Scribe" />
         <div className="overlay">
-          <div className="text"></div>
+          <div className="text">
+            Scribe
+            <br />
+            jQuery, Bulma, API
+          </div>
           <a
             href="https://github.com/pppreap/project1"
             rel="noreferrer"
             target="_blank"
-            className="scribe-proj"
-          ></a>
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faLink} />
+          </a>
         </div>
       </div>
       <div className="image-container">
-        <img src={OnTask} alt="" />
+        <img src={OnTask} alt="OnTask" />
         <div className="overlay">
-          <div className="text"></div>
+          <div className="text">
+            OnTask
+            <br />
+            MognoDB, React, Express
+          </div>
           <a
             href="https://github.com/Codeyg12/onTask"
             rel="noreferrer"
             target="_blank"
-            className="task-proj"
-          ></a>
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faLink} />
+          </a>
         </div>
       </div>
       <div className="image-container">
-        <img src={Employee} alt="" />
+        <img src={Employee} alt="Employee Tracker" />
         <div className="overlay">
-          <div className="text"></div>
+          <div className="text">
+            Employee Tracker
+            <br />
+            JavaScript, MySQL, Node
+          </div>
           <a
             href="https://github.com/Codeyg12/MySQL-employee-tracker"
             rel="noreferrer"
             target="_blank"
-            className="e-proj"
-          ></a>
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faLink} />
+          </a>
         </div>
       </div>
       <div className="image-container">
-        <img src={Hangman} alt="" />
+        <img src={Hangman} alt="Hangman" />
         <div className="overlay">
-          <div className="text"></div>
+          <div className="text">
+            Hangman
+            <br />
+            Vite, React
+          </div>
           <a
             href="https://github.com/Codeyg12/hangmanReact"
             rel="noreferrer"
             target="_blank"
-            className="hang-proj"
-          ></a>
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faLink} />
+          </a>
         </div>
       </div>
       <div className="image-container">
-        <img src={Book} alt="" />
+        <img src={Book} alt="Book Search" />
         <div className="overlay">
-          <div className="text"></div>
+          <div className="text">
+            Book Search
+            <br />
+            MongoDB, React, ApolloGraphQL
+          </div>
           <a
             href="https://github.com/Codeyg12/MERN-book-search-engine"
             rel="noreferrer"
             target="_blank"
-            className="book-proj"
-          ></a>
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faLink} />
+          </a>
         </div>
       </div>
     </div>
